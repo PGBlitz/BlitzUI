2e956f0e494bc1b2c44f533d085fbe59df30af88
Admin9705 <user@dunn.cloud>
2019-02-09 06:28:15 -0500

    update

--- templates/index.php
+++ templates/index.php
@@ -1,4 +1,29 @@
-<?php
-$output = shell_exec('ls -lart');
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+<style type="text/css">
+body {
+	background-color: #282E39;
+}
+.test {
+}
+</style>
+</head>
+
+<body>
+<table width="200" border="1">
+  <tbody>
+    <tr>
+      <td><img src="https://camo.githubusercontent.com/2d9a1f02588a2f0635117ab3e43dca618344c895/68747470733a2f2f706c657867756964652e636f6d2f77696b69706963732f6c6f676f2e706e67" width="75" height="75" alt=""/></td>
+      <td class="test"><strong>cat</strong></td>
+    </tr>
+  </tbody>
+</table>
+Hello, today is <?php $output = shell_exec('ls -lart');
 echo "<pre>$output</pre>";
-?>
+?>.
+
+</body>
+</html>
