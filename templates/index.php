7bf66d84973f4598caa448c3f4e1b311010452a0
Admin9705 <user@dunn.cloud>
2019-02-09 16:11:08 -0500

    update

--- templates/index.php
+++ templates/index.php
@@ -6,6 +6,7 @@
 <style type="text/css">
 body {
 	background-color: #282E39;
+	text-align: center;
 }
 .test {
 }
@@ -19,6 +20,17 @@ body {
       <td width="100">&nbsp;</td>
       <td align="center"><h1><strong style="color: #FFFFFF; font-family: Segoe, 'Segoe UI', 'DejaVu Sans', 'Trebuchet MS', Verdana, sans-serif;">PG User Interface (PG UI)</strong></h1></td>
       <td width="100" align="center"><img src="https://camo.githubusercontent.com/2d9a1f02588a2f0635117ab3e43dca618344c895/68747470733a2f2f706c657867756964652e636f6d2f77696b69706963732f6c6f676f2e706e67" width="75" height="75" alt=""/></td>
+    </tr>
+  </tbody>
+</table>
+<br>
+<table width="800" cellpadding="5">
+  <tbody>
+    <tr>
+      <td width="25%" style="text-align: center"><img src="https://camo.githubusercontent.com/6a9261977df6c815e4c1c0be4131cc243e7f9496/68747470733a2f2f706c657867756964652e636f6d2f77696b69706963732f6c6f676f2d666f72756d732e706e67" alt=""/></td>
+      <td width="25%" style="text-align: center"><img src="https://camo.githubusercontent.com/6a9261977df6c815e4c1c0be4131cc243e7f9496/68747470733a2f2f706c657867756964652e636f6d2f77696b69706963732f6c6f676f2d666f72756d732e706e67" alt=""/></td>
+      <td width="25%" style="text-align: center"><img src="https://camo.githubusercontent.com/6a9261977df6c815e4c1c0be4131cc243e7f9496/68747470733a2f2f706c657867756964652e636f6d2f77696b69706963732f6c6f676f2d666f72756d732e706e67" alt=""/></td>
+      <td width="25%" style="text-align: center"><img src="https://camo.githubusercontent.com/6a9261977df6c815e4c1c0be4131cc243e7f9496/68747470733a2f2f706c657867756964652e636f6d2f77696b69706963732f6c6f676f2d666f72756d732e706e67" alt=""/></td>
     </tr>
   </tbody>
 </table>
