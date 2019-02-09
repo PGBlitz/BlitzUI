3686dc3a9838b4965578d286fd5b101c021589c7
Admin9705 <user@dunn.cloud>
2019-02-09 14:52:31 -0500

    update

--- templates/index.php
+++ templates/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Untitled Document</title>
+<title>PG UI</title>
 <style type="text/css">
 body {
 	background-color: #282E39;
@@ -13,19 +13,15 @@ body {
 </head>
 
 <body>
-<table width="200" border="1">
-  <tbody>
-    <tr>
-      <td><img src="https://camo.githubusercontent.com/2d9a1f02588a2f0635117ab3e43dca618344c895/68747470733a2f2f706c657867756964652e636f6d2f77696b69706963732f6c6f676f2e706e67" width="75" height="75" alt=""/></td>
-      <td class="test">&nbsp;</td>
-    </tr>
-  </tbody>
-</table>
-	<input type="submit" class="button" name="select" value="select" />
 
-Hello, today is <?php $output = shell_exec('ls -lart');
+<p><img src="https://camo.githubusercontent.com/2d9a1f02588a2f0635117ab3e43dca618344c895/68747470733a2f2f706c657867756964652e636f6d2f77696b69706963732f6c6f676f2e706e67" width="75" height="75" alt=""/></p>
+<p>
+
+PG Version:
+<?php $output = shell_exec('cat /var/plexguide/pgclone.transport');
 echo "<pre>$output</pre>";
 ?>.
-	
+
+</p>
 </body>
 </html>
