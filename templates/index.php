6f3b3df498b8bd17dda9d78440d1a3c6ebcd3ebf
Admin9705 <user@dunn.cloud>
2019-02-09 17:32:12 -0500

    update

--- templates/index.php
+++ templates/index.php
@@ -46,14 +46,14 @@ body,td,th {
 </table>
 <br>
 
-<table width="90%" border="1" height="20" align="center" cellpadding="0" cellspacing="0">
+<table width="90%" border="1" height="20" align="center" cellpadding="4" cellspacing="0">
   <tbody>
     <tr>
       <td width="125" height="20" bgcolor="#000000" style="color: #F7F6F6; font-family: Segoe, 'Segoe UI', 'DejaVu Sans', 'Trebuchet MS', Verdana, sans-serif; font-weight: bold; font-size: medium;"> <span class="test"><strong>&nbsp;&nbsp;Edition&nbsp;&nbsp;</strong></span></td>
-      <td width="33%" height="20" class="test" style="color: #FFFFFF; font-family: Segoe, 'Segoe UI', 'DejaVu Sans', 'Trebuchet MS', Verdana, sans-serif;"><span class="test" style="color: #FFFFFF; font-family: Segoe, 'Segoe UI', 'DejaVu Sans', 'Trebuchet MS', Verdana, sans-serif; font-size: medium;">&nbsp;<?php $output = shell_exec('cat /plexguide/pg.transport');
+      <td width="33%" height="20" class="test" style="color: #FFFFFF; font-family: Segoe, 'Segoe UI', 'DejaVu Sans', 'Trebuchet MS', Verdana, sans-serif;"><?php $output = shell_exec('cat /plexguide/pg.transport');
 echo "<pre>$output</pre>";
 ?>
-      </span></td>
+      <span style="text-align: center"></span><span style="text-align: justify"></span></td>
       <td width="125" height="20" bgcolor="#000000" class="test" style="color: #FFFFFF; font-family: Segoe, 'Segoe UI', 'DejaVu Sans', 'Trebuchet MS', Verdana, sans-serif; font-size: medium;"><strong> &nbsp;&nbsp;Version&nbsp;&nbsp; </strong></td>
       <td width="34%" height="20" class="test" style="color: #FFFFFF; font-family: Segoe, 'Segoe UI', 'DejaVu Sans', 'Trebuchet MS', Verdana, sans-serif; font-size: medium;"><span class="test" style="color: #FFFFFF; font-family: Segoe, 'Segoe UI', 'DejaVu Sans', 'Trebuchet MS', Verdana, sans-serif; font-size: medium;">
         <?php $output = shell_exec('cat /plexguide/pg.number');
