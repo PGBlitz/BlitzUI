5f30dc5f36a257e73aba535721512f97f1a2c2c1
Admin9705 <user@dunn.cloud>
2019-02-10 00:53:24 -0500

    update

--- templates/index.php
+++ templates/index.php
@@ -219,7 +219,7 @@ echo "<pre>$output</pre>";
   <tbody>
     <tr>
       <td colspan="6" bgcolor="#000000" style="color: #F7F6F6; font-family: Segoe, 'Segoe UI', 'DejaVu Sans', 'Trebuchet MS', Verdana, sans-serif; font-weight: bold; font-size: medium;"><span class="test" style="color: #FFFFFF; font-family: Segoe, 'Segoe UI', 'DejaVu Sans', 'Trebuchet MS', Verdana, sans-serif; font-size: medium;">
-        <?php $output = shell_exec('tac /log/pgblitz.log | sed -e "/PG Blitz Log - Cycle/q"');
+        <?php $output = shell_exec('tac /plexguide/logs/pgblitz.log | sed -e "/PG Blitz Log - Cycle/q"');
 echo "<pre>$output</pre>";
 ?>
       </span></td>
