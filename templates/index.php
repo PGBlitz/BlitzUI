30c221ea0992dcf168d5acf020905a975c922e8a
Admin9705 <user@dunn.cloud>
2019-02-09 22:28:47 -0500

    update

--- templates/index.php
+++ templates/index.php
@@ -22,7 +22,7 @@ a:link {
 </head>
 
 <body text="#FFFFFF">
-<table width="100%" border="0" cellpadding="5" cellspacing="5" background="https://plexguide.com/wikipics/motherboard2.png">
+<table width="100%" border="0" cellpadding="0" cellspacing="0" background="https://plexguide.com/wikipics/motherboard2.png">
   <tbody>
     <tr>
       <td width="100">&nbsp;</td>
