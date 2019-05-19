ddb7bf5deca3dc83f553fccb05a45f7de2c6b869
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-05-19 16:14:30 +0200

    Initial scale
    From 1.0 to 0.5

--- templates/index.php
+++ templates/index.php
@@ -4,7 +4,7 @@
 <meta http-equiv="refresh" content="25" />
 <meta charset="UTF-8">
 <title>PG User Interface</title>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=0.5">
 <link rel="icon" type="image/png" href="https://i.imgur.com/T7OQsZz.png" sizes="32x32">
 <style type="text/css">
 body {
